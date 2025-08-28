d3c3ec172ca2c5a12960be5adf3e7267e6173513
Aidan <aidanlmcmurray@gmail.com>
2025-08-28 16:03:46 -0700

    ES Modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import posts from "./routes/posts.js";
 const port = process.env.PORT || 8000;
+
 
 const app = express();
 
@@ -17,35 +19,10 @@ const app = express();
 //     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 // })
 
-let posts = [
-  { id: 1, title: "Post One" },
-  { id: 2, title: "Post Two" },
-  { id: 3, title: "Post Three" },
-];
 
-// get all posts
-app.get("/api/posts", (req, res) => {
-  // console.log(req.query); // query params
-  const limit = parseInt(req.query.limit);
+// Routes
+app.use('/api/posts', posts);
 
-  if (!isNaN(limit) && limit > 0) {
-    return res.status(200).json(posts.slice(0, limit));
-  }
-
-  res.status(200).json(posts);
-});
-
-// get a single post
-app.get("/api/posts/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    return res.status(404).json({ message: `Post with id of ${id} not found` });
-  }
-
-  res.status(200).json(post);
-});
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
